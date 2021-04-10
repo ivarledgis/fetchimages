b2c88aec93a17150cf9655986d173971d755d2da
ivarledgis <ivarledgis90@gmail.com>
2021-04-10 20:21:46 +0545

    made extra function

--- app/Http/Controllers/ImageFetchController.php
+++ app/Http/Controllers/ImageFetchController.php
@@ -9,26 +9,16 @@ class ImageFetchController extends Controller
 {
     /**
      * Get all images from public/images folder
-     * 
-     * public_path gives the path of the public folder 
-     * glob function returns all the files inside that folder that matches the given pattern
-     * explode breaks string into an array 
-     * 
+     *
      * @return JSONResponse all_images
      */
     public function getImages()
     {
         try {
-            $imagesDir = public_path() . "/images/";
-            $images = glob($imagesDir . '*.{jpg,jpeg,png,gif}', GLOB_BRACE);
-            $allImages = [];
-            foreach ($images as $key => $image) {
-                $onlyImageName = explode($imagesDir, $image);
-                $imageLink = asset("/images/" . $onlyImageName[1]);
-                array_push($allImages, $imageLink);
-            }
 
-            return response()->json(['success_message' => 'random image', 'images' => $allImages], 200);
+            $allImages = $this->getImageUrl(false);
+
+            return response()->json(['success_message' => 'All Images', 'images' => $allImages], 200);
         } catch (Exception $th) {
             return response()->json(['error_message' => 'Something went wrong.'], 500);
         }
@@ -36,25 +26,41 @@ class ImageFetchController extends Controller
 
     /**
      * Get a random Image from public/images folder
-     * 
-     * public_path gives the path of the public folder 
-     * glob function returns all the files inside that folder that matches the given pattern
-     * explode breaks string into an array 
-     * 
+     *
      * @return JSONResponse rand_image
      */
     public function getRandImage()
     {
         try {
-            $imagesDir = public_path() . "/images/";
-            $images = glob($imagesDir . '*.{jpg,jpeg,png,gif}', GLOB_BRACE);
-            $randomImage = $images[array_rand($images)];
-            $onlyImageName = explode($imagesDir, $randomImage);
-            $imageLink = asset("/images/" . $onlyImageName[1]);
+            $imageLink = $this->getImageUrl();
 
-            return response()->json(['success_message' => 'random image', 'image' => $imageLink], 200);
+            return response()->json(['success_message' => 'random image', 'images' => $imageLink], 200);
         } catch (Exception $th) {
             return response()->json(['error_message' => 'Something went wrong.'], 500);
         }
+    }
+
+    /**
+     * public_path gives the path of the public folder 
+     * glob function returns all the files inside that folder that matches the given pattern
+     * explode breaks string into an array 
+     * 
+     * @return Array $image
+     */
+    private function getImageUrl($random = true)
+    {
+        $imagesDir = public_path() . "/images/";
+        $images = glob($imagesDir . '*.{jpg,jpeg,png,gif}', GLOB_BRACE);
+        if ($random) {
+            $images = [$images[array_rand($images)]];
+        }
+        $allImages = [];
+        foreach ($images as $key => $image) {
+            $onlyImageName = explode($imagesDir, $image);
+            $imageLink = asset("/images/" . $onlyImageName[1]);
+            array_push($allImages, $imageLink);
+        }
+
+        return $allImages;
     }
 }
